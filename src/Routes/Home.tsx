82cecb0df970762bdde3b91ce8fba42afcd5fd7d
asd0905 <asd0905@naver.com>
2023-05-13 01:32:42 +0900

    React JS 마스터클래스 (9.8 Slider part Two) feat.netflex-clone
    React JS 마스터클래스 (9.8 Slider part Two) feat.netflex-clone

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -7,6 +7,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const SWrapper = styled.div`
   background-color: black;
+  overflow-x: hidden;
 `;
 
 const Loading = styled.div`
@@ -25,6 +26,7 @@ const SBanner = styled.div<{ bhPhoto: string }>`
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
     ${(props) => "url(" + props.bhPhoto + ")" || "none"};
   background-size: cover;
+  background-position: center center;
 `;
 
 const STitle = styled.h2`
@@ -39,35 +41,40 @@ const SOverView = styled.p`
 
 const SSlider = styled.div`
   position: relative;
-  top: -100px;
+  top: -200px;
 `;
 
 const SRow = styled(motion.div)`
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 5px;
   grid-template-columns: repeat(6, 1fr);
   position: absolute;
   width: 100%;
 `;
 
-const SBox = styled(motion.div)`
+const SBox = styled(motion.div)<{ bgPhoto: string }>`
   background-color: #fff;
   height: 200px;
   font-size: 30px;
   color: red;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
 `;
 
 const rowVariants = {
   hidden: {
-    x: window.outerWidth,
+    x: document.body.clientWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth,
+    x: -document.body.clientWidth - 5,
   },
 };
+
+const offset = 6;
 
 function MovieBanner() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
@@ -75,33 +82,50 @@ function MovieBanner() {
     getMovies,
     {
       suspense: true,
+      refetchOnWindowFocus: false,
     }
   );
   console.log(data, isLoading);
   const [index, setIndex] = useState(0);
-  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (index === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   return (
     <>
       <SBanner
         onClick={increaseIndex}
-        bhPhoto={makeImagePath(data?.results[2].backdrop_path || "")}
+        bhPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
       >
-        <STitle>{data?.results[2].title}</STitle>
-        <SOverView>{data?.results[2].overview}</SOverView>
+        <STitle>{data?.results[0].title}</STitle>
+        <SOverView>{data?.results[0].overview}</SOverView>
       </SBanner>
       <SSlider>
-        <AnimatePresence>
+        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
           <SRow
             variants={rowVariants}
             initial="hidden"
             animate={"visible"}
             exit={"exit"}
-            transition={{ ease: "linear", duration: 5 }}
+            transition={{ ease: "linear", duration: 1 }}
             key={index}
           >
-            {[1, 2, 3, 4, 5, 6].map((i) => (
-              <SBox key={i}>{i}</SBox>
-            ))}
+            {data?.results
+              .slice(1)
+              .slice(offset * index, offset * index + offset)
+              .map((movie) => (
+                <SBox
+                  key={movie.id}
+                  bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                ></SBox>
+              ))}
           </SRow>
         </AnimatePresence>
       </SSlider>
