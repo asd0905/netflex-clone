b0398e76830b69a2203694877f6555c696286a9f
asd0905 <asd0905@naver.com>
2023-05-12 17:00:10 +0900

    React JS 마스터클래스 (9.6 Home Screen part Two) feat.netflex-clone
    React JS 마스터클래스 (9.6 Home Screen part Two) feat.netflex-clone

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,9 +1,67 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { IGetMoviesResult, getMovies } from "../api";
+import styled from "styled-components";
+import { Suspense } from "react";
+import { makeImagePath } from "../utills";
+
+const SWrapper = styled.div`
+	background-color: black;
+`;
+
+const Loading = styled.div`
+	height: 20vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const SBanner = styled.div<{ bhPhoto: string }>`
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding: 60px;
+	background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+		${(props) => "url(" + props.bhPhoto + ")" || "none"};
+	background-size: cover;
+`;
+
+const STitle = styled.h2`
+	font-size: 48px;
+	margin-bottom: 15px;
+`;
+
+const SOverView = styled.p`
+	font-size: 18px;
+	width: 50%;
+`;
+
+function MovieBanner() {
+	const { data, isLoading } = useQuery<IGetMoviesResult>(
+		["movies", "nowPlaying"],
+		getMovies,
+		{
+			suspense: true,
+		}
+	);
+	console.log(data, isLoading);
+	return (
+		<>
+			<SBanner bhPhoto={makeImagePath(data?.results[1].backdrop_path || "")}>
+				<STitle>{data?.results[1].title}</STitle>
+				<SOverView>{data?.results[1].overview}</SOverView>
+			</SBanner>
+		</>
+	);
+}
 
 function Home() {
-	const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
-	console.log(data, isLoading);
-	return <div style={{ height: "200vh" }}>home</div>;
+	return (
+		<SWrapper>
+			<Suspense fallback={<Loading>Loading...</Loading>}>
+				<MovieBanner></MovieBanner>
+			</Suspense>
+		</SWrapper>
+	);
 }
 export default Home;
--- src/api.ts
+++ src/api.ts
@@ -2,14 +2,44 @@ export const API_KEY = "80f770ab394bc4a45cdcf0d5a157354e";
 export const BASE_PATH = `https://api.themoviedb.org/3`;
 export const NOW_PLAYING_MOVIE = `${BASE_PATH}/movie/now_playing`;
 
-interface IMovieApiProps extends Record<string, string> {
+export interface IMovieApiProps extends Record<string, string> {
 	language: string;
 	region: string;
 	page: string;
 	api_key: string;
 }
 
-export async function getMovies() {
+export interface IGetMoviesResult {
+	dates: IDates;
+	page: number;
+	results: IMovie[];
+	total_pages: number;
+	total_results: number;
+}
+
+export interface IDates {
+	maximum: string;
+	minimum: string;
+}
+
+export interface IMovie {
+	adult: boolean;
+	backdrop_path: string;
+	genre_ids: number[];
+	id: number;
+	original_language: string;
+	original_title: string;
+	overview: string;
+	popularity: number;
+	poster_path: string;
+	release_date: string;
+	title: string;
+	video: boolean;
+	vote_average: number;
+	vote_count: number;
+}
+
+export async function getMovies(): Promise<IGetMoviesResult> {
 	const paramsObj = {
 		language: "en-US",
 		region: "kr",
